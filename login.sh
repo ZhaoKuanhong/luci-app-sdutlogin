3c66639f851536e879d2ee46c9b1d1d71e330fc1
宽宏 赵 <zhaokuanhong@live.cn>
2023-08-28 15:59:02 +0800

    修改ip获取方法

--- login.sh
+++ login.sh
@@ -56,7 +56,8 @@ function up(){
 	then
 
    	 echo "$(date "+%Y-%m-%d %H:%M:%S"):Not signed in yet" >> ${logfile}
-   	 ip=$(ifconfig eth0 | grep 'inet addr:' | grep -oE '([0-9]{1,3}.){3}.[0-9]{1,3}' | head -n 1)
+   	 ip=$(ubus call network.interface.wan status | grep '\"address\"\: \"' | grep -oE '([0-9]{1,3}.){3}.[0-9]{1,3}')
+
 
    	 curl "http://111.17.200.130:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=${username}&user_password=${password}&wlan_user_ip=${ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2.1&terminal_type=1&lang=zh-cn&v=6915&lang=zh" >> ${logfile} 2>&1
 	fi
